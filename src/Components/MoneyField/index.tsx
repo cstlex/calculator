ed094572127d85249f105e8b0cc368b19cf2945c
MinSoo Kim <minsoo8449@gmail.com>
2022-09-06 22:14:47 -0400

    fixed money field number formatting issue

--- src/Components/MoneyField/index.tsx
+++ src/Components/MoneyField/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNumberState } from 'Utils/NumberState'
 import cn from 'classnames'
+import 'Utils/Extensions/NumberUtils'
 
 type Props = {
     value: number
@@ -20,12 +21,22 @@ export default function MoneyField({
         setShowingValue(`$${finalValue.formatted}`)
     }, [finalValue])
 
+    const onEditFinished = (text: string) => {
+        const filtered = text.replace(/[^0-9.]/g, '')
+        const numberValue = parseFloat(filtered)
+        if (Number.isNaN(numberValue) || numberValue === finalValue) {
+            setShowingValue(`$${finalValue.formatted}`)
+        } else {
+            setFinalValue(filtered)
+        }
+    }
+
     return (
         <input
             className={cn('p-2 h-8', className)}
             value={showingValue}
             onChange={(e) => setShowingValue(e.target.value)}
-            onBlur={(e) => setFinalValue(e.target.value)}
+            onBlur={(e) => onEditFinished(e.target.value)}
         />
     )
 }
