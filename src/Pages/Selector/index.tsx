421d28c107f6312f818920e65ad2da58e7bf7e9e
MinSoo Kim <minsoo8449@gmail.com>
2022-09-06 22:15:45 -0400

    selector styling fixed

--- src/Pages/Selector/index.tsx
+++ src/Pages/Selector/index.tsx
@@ -5,13 +5,13 @@ export default function Selector() {
     return (
         <div className="flex flex-col items-center space-y-5">
             <Link
-                className="text-2xl rounded-2xl bg-accent-background-light dark:bg-accent-background-dark py-2 w-1/2 flex justify-center"
+                className="text-2xl rounded-2xl bg-accent-background-light dark:bg-accent-background-dark py-2 w-1/2 flex justify-center text-center"
                 to="/tax"
             >
                 Income Tax Calculator
             </Link>
             <Link
-                className="text-2xl rounded-2xl bg-accent-background-light dark:bg-accent-background-dark py-2 w-1/2 flex justify-center"
+                className="text-2xl rounded-2xl bg-accent-background-light dark:bg-accent-background-dark py-2 w-1/2 flex justify-center text-center"
                 to="/loan"
             >
                 Loan Repayment Calculator
